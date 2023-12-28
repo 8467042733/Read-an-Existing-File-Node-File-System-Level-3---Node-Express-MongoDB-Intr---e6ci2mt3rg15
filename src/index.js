d2011417af42a2eea7e6e225a51221325270f360
8467042733 <rupeshpal25112004@gmail.com>
2023-12-28 13:25:11 +0530

    add some code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
-  
+  fs.readFile(fileName,function(err,data){
+    if(err) throw err;
+    console.log(data);
+  })
 };
-
+reader(fileName);
 
 module.exports =  reader ;
